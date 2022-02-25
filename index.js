6417341e49f8b98ba968f29f425a67e859305079
QualityPre <qualitypre@protonmail.com>
2022-02-25 14:31:25 +0000

    uibuild

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+const Note = require("./models/note");
+
 app.use(express.json());
 app.use(express.static("build"));
 const cors = require("cors");
 
 app.use(cors());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello Pooface!</h1>");
 });
 
+
+
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  // response.statusMessage = "Current password does not match";
-  const note = notes.find((note) => {
-    return note.id === id;
+  
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
   });
-
-  if (note) response.json(note);
-  else response.status(404).end();
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -61,22 +45,22 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const requestLogger = (request, response, next) => {
@@ -93,7 +77,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
