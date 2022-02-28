79ca19a1bcbc51fda65ba4a33520f215c66fab0d
QualityPre <qualitypre@protonmail.com>
2022-02-28 08:43:59 +0000

    uibuild

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const app = express();
 require("dotenv").config();
 const Note = require("./models/note");
 
-app.use(express.json());
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
 app.use(express.static("build"));
+app.use(express.json());
+app.use(requestLogger);
+
 const cors = require("cors");
 
 app.use(cors());
 
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello Pooface!</h1>");
 });
-
-
 
 app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
@@ -24,58 +31,79 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-app.post("/api/notes", (request, response) => {
+  const note = {
+    content,
+    important,
+  };
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-app.use(requestLogger);
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
-
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if ((error.name = "ValidationError")) {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
