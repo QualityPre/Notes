60cc97910263ca4a8e7e8ff88a33370fa90ab539
QualityPre <qualitypre@protonmail.com>
2022-03-09 07:56:06 +0000

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose')
 
-// const url = process.env.MONGODB_URI
-
-// console.log('connecting to', url)
-
-// mongoose
-//   .connect(url)
-//   .then(() => {
-//     console.log('connected to MongoDB')
-//   })
-//   .catch((error) => {
-//     console.log('error connecting to MongoDB:', error.message)
-//   })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
@@ -24,6 +11,10 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
 noteSchema.set('toJSON', {
